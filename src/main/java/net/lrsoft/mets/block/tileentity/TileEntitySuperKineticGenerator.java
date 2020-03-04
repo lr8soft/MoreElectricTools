c4963aec17db3a014ff80f6716e167c04cea203d
LT_lrsoft <lrsoft@hotmail.com>
2020-03-04 10:54:25 +0800

    v0.34release

---
+++ src/main/java/net/lrsoft/mets/block/tileentity/TileEntitySuperKineticGenerator.java
@@ -0,0 +1,14 @@
+package net.lrsoft.mets.block.tileentity;
+
+import ic2.core.block.generator.tileentity.TileEntityKineticGenerator;
+import ic2.core.init.MainConfig;
+import ic2.core.util.ConfigUtil;
+
+public class TileEntitySuperKineticGenerator extends TileEntityKineticGenerator {
+	private final double euPerKu = 2.0D * ConfigUtil.getFloat(MainConfig.get(), "balance/energy/generator/Kinetic");
+	@Override
+	protected double getMultiplier() 
+	{
+		 return this.euPerKu;
+	}
+}
