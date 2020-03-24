ba670e75bf0761a3d664c0705b82eaa8d5a7db47
LT_lrsoft <lrsoft@hotmail.com>
2020-03-24 19:05:09 +0800

    v1.23pre1

--- src/main/java/net/lrsoft/mets/crop/CropManager.java
+++ src/main/java/net/lrsoft/mets/crop/CropManager.java
@@ -1,19 +1,29 @@
 package net.lrsoft.mets.crop;
 
+import net.lrsoft.mets.MoreElectricTools;
 import net.lrsoft.mets.manager.BlockManager;
 import net.lrsoft.mets.manager.ItemManager;
 import net.minecraft.block.Block;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemSeeds;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.event.RegistryEvent.Register;
 
 public class CropManager {
-	public static Block ironRichRrop = new  IronRichCrop();
+	public static Block ironRichRrop = new IronRichCrop();
+	public static Item  ironRichSeed;
+	static 
+	{
+		ironRichSeed = getCropSeed(ironRichRrop, Blocks.FARMLAND, "iron_rich_seed");//new ItemSeeds(ironRichRrop, Blocks.FARMLAND);
+	}
 	public static void onItemInit(Register<Item> event)
 	{
-		event.getRegistry().register(new ItemBlock(ironRichRrop).setRegistryName(ironRichRrop.getRegistryName()));
+		//event.getRegistry().register(new ItemSeeds(ironRichRrop, Blocks.FARMLAND).
+		//		setRegistryName(MoreElectricTools.MODID, "iron_rich_seed").setUnlocalizedName("iron_rich_seed"));
+		event.getRegistry().register(ironRichSeed);
 	}
 	
 	public static void onBlockInit(Register<Block> event)
@@ -30,9 +40,14 @@ public class CropManager {
 	
 	public static void onModelInit()
 	{
-		//ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(BlockManager.niobiumOre), 0, new ModelResourceLocation(BlockManager.niobiumOre.getRegistryName(),"normal"));
+		ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(ironRichRrop), 0, new ModelResourceLocation(ironRichRrop.getRegistryName(),"normal"));
 		//	ModelLoader.setCustomModelResourceLocation(ItemManager.tacticalLaserSubmachineGun, 0,
 		//new ModelResourceLocation(ItemManager.tacticalLaserSubmachineGun.getRegistryName(), "inventory"));
+	}
+	
+	private static Item getCropSeed(Block crop, Block place, String itemName) 
+	{
+		return new ItemSeeds(crop, place).setRegistryName(MoreElectricTools.MODID, itemName).setUnlocalizedName(itemName);
 	}
 
 }
