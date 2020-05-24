802bd277fa099a1f0e73f33f291038f896ffae1f
LT_lrsoft <lrsoft@hotmail.com>
2020-05-24 20:01:04 +0800

    v1.62release

--- src/main/java/net/lrsoft/mets/manager/VillageManager.java
+++ src/main/java/net/lrsoft/mets/manager/VillageManager.java
@@ -3,6 +3,7 @@ package net.lrsoft.mets.manager;
 import java.util.List;
 import java.util.Random;
 
+import ic2.api.item.IC2Items;
 import net.lrsoft.mets.MoreElectricTools;
 import net.lrsoft.mets.item.crafting.ItemCraftingManager;
 import net.lrsoft.mets.structure.EngineerLodge;
@@ -38,12 +39,24 @@ public class VillageManager {
 	public static void onVillagerProfessionRegistration(RegistryEvent.Register<VillagerRegistry.VillagerProfession> event) {
 	    VillagerRegistry.VillagerCareer smelter = new VillagerRegistry.VillagerCareer(engineer, "smelter");
 	    smelter.addTrade(1, new EntityVillager.ListItemForEmeralds(new ItemStack(ItemCraftingManager.titanium_ingot), 
+	    		new PriceInfo(5, 6)));
+	    smelter.addTrade(1, new EntityVillager.ListItemForEmeralds(IC2Items.getItem("ingot", "bronze"), 
 	    		new PriceInfo(3, 6)));
 	    smelter.addTrade(2, new EntityVillager.ListItemForEmeralds(new ItemStack(ItemCraftingManager.niobium_titanium_ingot), 
-	    		new PriceInfo(20, 24)));
-	    smelter.addTrade(3, new EntityVillager.ListItemForEmeralds(new ItemStack(ItemCraftingManager.super_circuit), 
-	    		new PriceInfo(40, 45)));
+	    		new PriceInfo(24, 36)));
 
+	    
+	    VillagerRegistry.VillagerCareer toolTrader = new VillagerRegistry.VillagerCareer(engineer, "toolTrader");
+	    toolTrader.addTrade(1, new EntityVillager.ListItemForEmeralds(IC2Items.getItem("re_battery"), 
+	    		new PriceInfo(12, 24)));
+	    toolTrader.addTrade(1, new EntityVillager.ListItemForEmeralds(IC2Items.getItem("crafting", "circuit"), 
+	    		new PriceInfo(6, 16)));
+	    
+	    toolTrader.addTrade(2, new EntityVillager.ListItemForEmeralds(new ItemStack(ItemManager.electricSubmachineGun), 
+	    		new PriceInfo(42, 64)));
+	    toolTrader.addTrade(2, new EntityVillager.ListItemForEmeralds(new ItemStack(ItemManager.electricLighter), 
+	    		new PriceInfo(42, 64)));
+	    
 	    event.getRegistry().register(engineer);
 	    VillagerRegistry.instance().registerVillageCreationHandler(new EngineerLodgeCreationHandler());
 	    MapGenStructureIO.registerStructureComponent(EngineerLodge.class, "MetsEL");
